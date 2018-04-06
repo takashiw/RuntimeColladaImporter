656ef94b7a5d038e6b729e416c9511ec9a9511c8
iTwicKz <twickes32@ufl.edu>
2018-04-06 08:50:38 -0400

    Created AssetBundle method for saving and loading models

---
+++ Assets/EditorScript/SaveAssetBundles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SaveAssetBundles : EditorWindow {
+
+	[MenuItem("Window/Save Asset Bundles 😍")]
+	public static void Open() {
+		BuildPipeline.BuildAssetBundles("Assets/AssetBundles/", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSX);
+		AssetDatabase.Refresh();
+	}
+}
